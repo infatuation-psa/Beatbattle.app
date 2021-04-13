67833a044ab763c0e8ee897d7d16e6620e710070
Alec MacDonald <ratchet132@gmail.com>
2021-04-13 04:11:03 -0400

    Open external links in a new tab.

--- src/js/beatbattle.js
+++ src/js/beatbattle.js
@@ -1,4 +1,9 @@
 window.addEventListener('load',()=>{
   const observer = lozad();
   observer.observe();
+  
+  $('.battle-rules p').children('a').attr('target', function() {
+    if(this.host == location.host) return '_self'
+    else return '_blank'
+  });
 })
